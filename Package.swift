7dcabda5753c598d580d6e95e36221164c3a222a
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2020-09-21 16:52:57 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/cuatrotresdosuno/Arrow", .exact("5.1.3")),
         .package(url: "https://github.com/cuatrotresdosuno/Then", .exact("5.1.4")),
-        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.2.0")
+        .package(url: "https://github.com/Alamofire/Alamofire", from: "5.1.0")
     ],
     targets: [
         .target(name: "ws", dependencies:["Arrow", "Then", "Alamofire"]),
