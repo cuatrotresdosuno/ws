bd098338c852103d9ffe9f8b401ff7eb6cbef0e3
Joshua Kaunert <44586402+jkaunert@users.noreply.github.com>
2020-09-21 16:38:26 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,11 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "ws",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v9)],
     products: [.library(name: "ws", targets: ["ws"])],
     dependencies: [
-        .package(url: "https://github.com/freshOS/Arrow", .exact("5.1.2")),
-        .package(url: "https://github.com/Alamofire/Alamofire", .exact("4.9.1"))
+        .package(url: "https://github.com/freshOS/Arrow", .exact("5.1.3")),
+        .package(url: "https://github.com/Alamofire/Alamofire", .exact("5.2.0"))
     ],
     targets: [
         .target(name: "ws", dependencies:["Arrow", "Alamofire"]),
